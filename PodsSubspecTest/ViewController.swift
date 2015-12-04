27765f69459bf919e8eddc8d52d1f7b577d64fe6
Peter Livesey <plivesey@linkedin.com>
2015-12-04 09:21:27 -0800

    Initial Commit

---
+++ PodsSubspecTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodsSubspecTest
+//
+//  Created by Peter Livesey on 12/4/15.
+//  Copyright © 2015 PeterLivesey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
